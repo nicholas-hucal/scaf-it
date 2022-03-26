184e0c82d266b4c980c1b5e78753953b369669fb
Nicholas Hucal <nhucal@mac.com>
2022-03-26 11:15:09 -0600

    minor changes to incorporate hooks

--- src/pages/Editor/Editor.js
+++ src/pages/Editor/Editor.js
@@ -1,10 +1,26 @@
 import './Editor.scss';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import LoginButton from '../../components/LoginButton/LoginButton';
 import SiteLink from '../../components/SiteLink/SiteLink';
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-const Editor = ({isLoggedIn}) => {
-    if(!isLoggedIn) {
+const Editor = ({ isLoggedIn }) => {
+
+    useEffect(() => {
+        console.log('useEffect')
+        // axios
+        //     .get(`${SERVER_URL}`)
+        //     .then(res => {
+        //         console.log(res.data);
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+    }, []);
+
+
+    if (!isLoggedIn) {
         return <LoginButton title='Please Login' />
     }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
 import './Home.scss';
 import background from '../../assets/images/scaffold-bg.jpg';
 import logo from '../../assets/logos/logo@2x.png';
 import SiteLink from '../../components/SiteLink/SiteLink';
 import Triangle from '../../components/Triangle/Triangle';
 
-class Home extends Component {
-
-    goToEditor
-
-    render() {
-        return (
-            <div className='home'>
-                <section className='home__hero'>
-                    <div className='home__hero-left'>
-                        <img className='home__logo' src={logo} alt="scafit logo" />
-                        <h1 className='home__heading'>Easy component scaffolding for efficient React development</h1>
-                        <SiteLink to='/editor' text='to the editor'/>
+const Home = () => {
+    return (
+        <div className='home'>
+            <section className='home__hero'>
+                <div className='home__hero-left'>
+                    <img className='home__logo' src={logo} alt="scafit logo" />
+                    <h1 className='home__heading'>Easy component scaffolding for efficient React development</h1>
+                    <SiteLink to='/editor' text='to the editor' />
+                </div>
+                <div className='home__hero-right'>
+                    <img className='home__background-image' src={background} alt="scaffolding background" />
+                    <div className='home__triangle'>
+                        <Triangle />
+                        <Triangle last={true} />
                     </div>
-                    <div className='home__hero-right'>
-                        <img className='home__background-image' src={background} alt="scaffolding background" />
-                        <div className='home__triangle'>
-                            <Triangle />
-                            <Triangle last={true} />
-                        </div>
-                    </div>
-                </section>
-            </div>
-        );
-    }
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default Home;
